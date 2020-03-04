62da6a3a5dba5a8fff049f8a7170992f0c1189fa
Sean Melton <seanlape@gmail.com>
2020-03-04 03:51:03 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,6 +1,6 @@
 def countdown(counter)
   while counter > 0
-    puts "#{counter} SECOND (S)!"
+    puts "#{counter} SECOND(S)!"
     counter -= 1
   end
   return "HAPPY NEW YEAR!"
@@ -9,7 +9,7 @@ end
 def countdown_with_sleep(counter)
   while counter > 0
    sleep 1
-    puts "#{counter} SECON (S)!"
+    puts "#{counter} SECON(S)!"
     counter -= 1
   end
   return "HAPPY NEW YEAR!"
